47b9d6abc46663564488acf25b888a7157528b78
Brian Olencki <brian@olencki.com>
2020-12-17 14:14:49 -0500

    Add missing error to exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import UnauthorizedError from './errors/UnauthorizedError'
 import ForbiddenError from './errors/ForbiddenError'
 import InvalidRoleError from './errors/InvalidRoleError'
+import DuplicateUserError from './errors/DuplicateUserError'
 
 export { EasyJWTAuth } from './EasyJWTAuth'
 export const errors = {
   UnauthorizedError,
   ForbiddenError,
-  InvalidRoleError
+  InvalidRoleError,
+  DuplicateUserError
 }
