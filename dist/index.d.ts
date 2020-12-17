3cb08efe113246f3e7f59378f28fe2046ac7a021
Brian Olencki <brian@olencki.com>
2020-12-17 14:15:09 -0500

    v1.1.1 build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,11 @@
 import UnauthorizedError from './errors/UnauthorizedError'
 import ForbiddenError from './errors/ForbiddenError'
 import InvalidRoleError from './errors/InvalidRoleError'
+import DuplicateUserError from './errors/DuplicateUserError'
 export { EasyJWTAuth } from './EasyJWTAuth'
 export declare const errors: {
   UnauthorizedError: typeof UnauthorizedError
   ForbiddenError: typeof ForbiddenError
   InvalidRoleError: typeof InvalidRoleError
+  DuplicateUserError: typeof DuplicateUserError
 }
